bbd8dfd996347844896565020da7dd71b8687337
Miguel Aragón <miguel.aragon@base22.com>
2016-06-22 15:33:11 -0500

    Add gulp task "build:docker-image"

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const spawn = require( "child_process" ).spawn;
 const gulp = require( "gulp" );
 const util = require( "gulp-util" );
 const runSequence = require( "run-sequence" );
@@ -21,13 +22,39 @@ const sourcemaps = require( "gulp-sourcemaps" );
 const webserver = require( "gulp-webserver" );
 
 const argv = require( "yargs" )
-	.usage( "Usage: [-p profile]" )
+	.usage( "Usage: $0 [-p profile] [--use-env]" )
+
 	.describe( "p", "Active profile to load configuration from" )
 	.alias( "p", "profile" )
 	.default( "p", "local" )
+
+	.describe( "use-env", "Use environment variables instead of using profile based configuration" )
+
+	.describe( "image-name", "Docker image to build" )
+	.default( "image-name", "carbon-workbench" )
+
+	.describe( "image-version", "Docker image version" )
+	.default( "image-version", "latest" )
+
 	.argv;
-const profile = argv.p;
-const profileConfig = require( "./config/" + profile );
+
+let profileConfig;
+if( argv[ "use-env" ] ) {
+	validateEnv();
+	profileConfig = {
+		"angular": {
+			"debug": "DEBUG" in process.env && process.env.DEBUG
+		},
+		"url": {
+			"base": process.env.BASE
+		},
+		"carbon": {
+			"domain": process.env.CARBON_HOST
+		}
+	};
+} else {
+	profileConfig = require( "./config/" + argv.p );
+}
 
 const config = {
 	source: {
@@ -50,6 +77,8 @@ const config = {
 		]
 	}
 };
+
+gulp.task( "default", [ "build" ] );
 
 gulp.task( "ts-lint", () => {
 	return gulp.src( config.source.typescript )
@@ -105,14 +134,14 @@ gulp.task( "build-semantic", () => {
 gulp.task( "copy-semantic", [ "build-semantic" ], () => {
 	return gulp.src( "src/semantic/dist/**/*", {
 		base: "src/semantic/dist"
-	} ).pipe( gulp.dest( "dist/dist/assets/semantic" ) );
+	} ).pipe( gulp.dest( "dist/site/assets/semantic" ) );
 } );
 
 // TODO: Minify files
 gulp.task( "copy-assets", [ "copy-node-dependencies" ], () => {
 	return gulp.src( "src/assets/**/*", {
 		base: "src/assets"
-	} ).pipe( gulp.dest( "dist/dist/assets" ) );
+	} ).pipe( gulp.dest( "dist/site/assets" ) );
 } );
 
 gulp.task( "copy-node-dependencies", () => {
@@ -171,3 +200,73 @@ gulp.task( "build", [ "clean:dist" ], ( done ) => {
 		done
 	);
 } );
+
+gulp.task( "build:docker-image", ( done ) => {
+	runSequence(
+		"build:docker-image|copy:dockerfile",
+		"build:docker-image|build:image",
+		"build:docker-image|clean:dockerfile",
+		done
+	);
+} );
+
+gulp.task( "build:docker-image|copy:dockerfile", () => {
+	return gulp.src( "build/Dockerfile" )
+		.pipe( gulp.dest( "../" ) );
+} );
+
+gulp.task( "build:docker-image|build:image", ( done ) => {
+	let buildProcess = spawn( `docker`, [ `build`, `--tag`, `${argv[ "image-name" ]}:${argv[ "image-version" ]}`, `.` ], { cwd: getParentDirectory() } );
+
+	buildProcess.stdout.setEncoding( "utf8" );
+	buildProcess.stderr.setEncoding( "utf8" );
+
+	buildProcess.stdout.on( "data", logStdout );
+	buildProcess.stderr.on( "data", logStderr );
+	//buildProcess.stdout.pipe( process.stdout );
+	//buildProcess.stderr.pipe( process.stderr );
+	buildProcess.on( "close", ( code ) => {
+		if( code !== 0 ) done( "Docker build command failed" );
+		else done();
+	} );
+} );
+
+gulp.task( "build:docker-image|clean:dockerfile", () => {
+	return del( "../Dockerfile", { force: true } );
+} );
+
+function validateEnv() {
+	let valid = true;
+	if( ! ( "WORKBENCH_BASE" in process.env ) ) {
+		valid = false;
+		console.error( `ERROR: ENV["WORKBENCH_BASE"] was not defined. Please define the URL base the workbench is going to have` );
+	}
+	if( ! ( "CARBON_HOST" in process.env ) ) {
+		valid = false;
+		console.error( `ERROR: ENV["CARBON_HOST"] was not defined. Please define the Carbon LDP host the workbench is going to work with` );
+	}
+
+	if( ! valid ) process.exit( 1 );
+}
+
+function getParentDirectory( levels ) {
+	levels = levels ? levels : 1;
+
+	let directories = __dirname.split( "/" );
+
+	while( levels != 0 && directories.length > 1 ) {
+		directories.splice( - 1, 1 );
+		levels --;
+	}
+
+	if( directories.length == 1 ) return "/";
+	else return directories.join( "/" );
+}
+
+function logStdout( buffer ) {
+	console.log( buffer.toString() );
+}
+
+function logStderr( buffer ) {
+	console.error( buffer.toString() );
+}
