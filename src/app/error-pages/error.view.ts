8091d02bbc9a4c3dcf6b6e993b0c30a23d51f05a
Miguel Aragón <miguel.aragon@base22.com>
2016-09-28 12:51:46 -0500

    Add missing "/error" view

---
+++ src/app/error-pages/error.view.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import { Router } from "@angular/router";
+
+import Carbon from "carbonldp/Carbon";
+
+import { activeContext } from "angular2-carbonldp/boot";
+
+import { BackgroundVideoComponent } from "app/error-pages/background-video.component";
+
+import template from "./error.view.html!text";
+import style from "./error.view.css!text";
+
+@Component( {
+	selector: "div.cw-error-view",
+	template: template,
+	styles: [ style ],
+	directives: [ BackgroundVideoComponent ],
+} )
+export class ErrorView implements OnInit, AfterViewInit {
+	private error:any;
+	private errorType:string;
+
+	constructor( private router:Router, private carbon:Carbon ) {}
+
+	ngOnInit():void {
+		activeContext.promise.then( () => {
+			// The active context was successfully loaded, the user must have landed here by visiting the direct URL
+			// Let's redirect him to the home page
+			this.router.navigate( [ "/" ] );
+		} );
+	}
+
+	ngAfterViewInit():void {
+		activeContext.promise.catch( ( error ) => {
+			this.error = error;
+			this.errorType = "requestID" in this.error ? this.error.name : null;
+
+			if( this.errorType === null ) console.error( this.error );
+		} );
+	}
+}
+
+export default ErrorView;
