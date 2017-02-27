ca8acc6d7d5ddb2cc04101180101f55355aec98f
Alvaro Contreras <alvaro.contreras@base22.com>
2017-02-27 16:09:29 -0600

    Add config.ts file Remove config.ts from he lis of gitignore

---
+++ src/app/config.ts
@@ -0,0 +1,4 @@
+export const CARBON_PROTOCOL:string = process.env.CARBON.protocol;
+export const CARBON_DOMAIN:string = process.env.CARBON.domain;
+export const DEBUG:boolean = process.env.ENV === "production";
+export const URL_BASE:string = "";
