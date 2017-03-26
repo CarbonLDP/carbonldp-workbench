98e33d86501cbf75301259cf1fc32822835e04d2
Alvaro Contreras <alvaro.contreras@base22.com>
2017-03-26 17:37:47 -0600

    Change URL_BASE to BASE_URL and add missin $ to ENV

--- scripts/write-global-variables.sh
+++ scripts/write-global-variables.sh
@@ -5,9 +5,9 @@
 CARBON_PROTOCOL=${CARBON_PROTOCOL="http"}
 CARBON_HOST=${CARBON_HOST="localhost:8000"}
 ENV=${ENV="production"}
-BASE_URL=${URL_BASE="/"}
+BASE_URL=${BASE_URL="/"}
 
 sed -i 's|--CARBON_PROTOCOL--|'"$CARBON_PROTOCOL"'|g' $1
 sed -i 's|--CARBON_HOST--|'"$CARBON_HOST"'|g' $1
-sed -i 's|--ENV--|'"ENV"'|g' $1
+sed -i 's|--ENV--|'"$ENV"'|g' $1
 sed -i 's|--BASE_URL--|'"$BASE_URL"'|g' $1
