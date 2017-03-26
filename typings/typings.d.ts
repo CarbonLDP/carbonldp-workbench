feb6940caed3aca588e9044f1de746249d02751b
Alvaro Contreras <alvaro.contreras@base22.com>
2017-03-26 17:19:31 -0600

    Remove unused typings

--- typings/typings.d.ts
+++ typings/typings.d.ts
@@ -2,13 +2,3 @@
 /// <reference path="./custom/highlightjs/index.d.ts" />
 /// <reference path="./custom/jstree/index.d.ts" />
 /// <reference path="./custom/semantic-ui/index.d.ts" />
-
-declare module "*.html!" {
-	let value:string;
-	export default value;
-}
-
-declare module "*!text" {
-	let value:string;
-	export default value;
-}
