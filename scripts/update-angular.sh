d9fd26711dd17de87edeee06ab4091a8f864bbdc
Alvaro Contreras <alvaro.contreras@base22.com>
2017-03-26 19:32:21 -0600

    Remove update-angular.sh script

--- scripts/update-angular.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-die () {
-    echo >&2 "$@"
-    exit 1
-}
-
-[ "$#" -eq 1 ] || die "1 argument required, $# provided. Please provide only the version you want to install"
-
-jspm install npm:@angular/core@$1 npm:@angular/common@$1 npm:@angular/compiler@$1 npm:@angular/forms@$1 npm:@angular/http@$1 npm:@angular/platform-browser@$1 npm:@angular/platform-browser-dynamic@$1
