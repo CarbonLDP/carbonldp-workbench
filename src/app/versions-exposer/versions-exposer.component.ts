357c3b64fb78fb40e0ecaccd309fb42ee47d1ebc
Alvaro Contreras <alvaro.contreras@base22.com>
2017-03-27 13:15:28 -0600

    Add popup to display no platform error

--- src/app/versions-exposer/versions-exposer.component.ts
+++ src/app/versions-exposer/versions-exposer.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef } from "@angular/core";
 import { Carbon } from "carbonldp/Carbon";
 import * as APIDescription from "carbonldp/APIDescription";
+import * as $ from "jquery";
 
 @Component( {
 	selector: "versions-exposer",
@@ -8,22 +9,35 @@ import * as APIDescription from "carbonldp/APIDescription";
 	styleUrls: [ "./versions-exposer.component.scss" ],
 } )
 export class VersionsExposerComponent {
+	private element:ElementRef;
+	private $element:JQuery;
+	private carbon:Carbon;
 
 	carbonldpSDK:string = "???";
 	carbonldpPanel:string = "???";
 	carbonldpWorkbench:string = "???";
 	carbonldpPlatform:string = "???";
+	carbonldpURL:string = "";
 
-	constructor( private carbon:Carbon ) {
+	constructor( element:ElementRef, carbon:Carbon ) {
+		this.element = element;
+		this.carbon = carbon;
+	}
+
+	ngOnInit():void {
+		this.$element = $( this.element.nativeElement );
 		this.carbonldpSDK = process.env.PACKAGES[ "carbonldp" ];
 		this.carbonldpPanel = process.env.PACKAGES[ "carbonldp-panel" ];
 		this.carbonldpWorkbench = process.env.PACKAGES[ "carbonldp-workbench" ];
+		this.carbonldpURL = this.carbon.getSetting( "http.ssl" ) ? "https" : "http" + "://" + this.carbon.getSetting( "domain" );
 		this.getApiVersion();
 	}
 
 	getApiVersion():void {
 		this.carbon.getAPIDescription().then( ( apiDescription:APIDescription.Class ) => {
 			this.carbonldpPlatform = apiDescription.version;
+		} ).catch( ( error ) => {
+			this.$element.find( "span.platform" ).popup( { hideOnScroll: false } ).popup( "show" );
 		} );
 	}
 }
