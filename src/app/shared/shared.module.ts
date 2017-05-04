3a46897dc352de9266bf2486f6a8f7de41a144a2
Alvaro Contreras <alvaro.contreras@base22.com>
2017-05-04 17:20:31 -0500

    Move messages-area to SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,27 +1,47 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 
 // Components
+import { MessageComponent } from "./messages-area/message.component";
+import { MessagesAreaComponent } from "./messages-area/messages-area.component";
+import { ErrorLabelComponent } from "./messages-area/error/error-label.component";
 
 
 // Modules
 import { DirectivesModule } from "./directives/directives.module";
 import { SemanticModule } from "./semantic/semantic.module";
 
+// Services
+import { MessagesAreaService } from "./messages-area/messages-area.service";
 
 @NgModule( {
 	imports: [
-		BrowserModule,
-
+		CommonModule,
 	],
-	declarations: [],
+	declarations: [
+		MessageComponent,
+		MessagesAreaComponent,
+		ErrorLabelComponent
+	],
 	providers: [],
 	exports: [
+		MessageComponent,
+		MessagesAreaComponent,
+		ErrorLabelComponent,
+
 		DirectivesModule,
 		SemanticModule
 	]
 } )
 
 export class SharedModule {
+
+	static forRoot():ModuleWithProviders {
+		return {
+			ngModule: SharedModule,
+			providers: [ MessagesAreaService ]
+		};
+	}
+
 }
