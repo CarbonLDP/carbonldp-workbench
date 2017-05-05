bf8f4c6ea5dbcf15661bad229a052b5b60bdd3ca
Alvaro Contreras <alvaro.contreras@base22.com>
2017-05-04 21:47:51 -0500

    Add AppDashboardView to AppContentModule

---
+++ src/app/my-apps/app-content/dashboard/dashboard.view.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+import "semantic-ui/semantic";
+
+@Component( {
+	selector: "cw-dashboard-view",
+	template: "<h3>Dashboard View</h3>",
+	styles: [ ":host { display: block; }" ],
+} )
+export class DashboardView {
+}
+
