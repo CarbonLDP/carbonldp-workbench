e7532e950595c9edf3dfd26ff1d273e467fca867
Alvaro Contreras <alvaro.contreras@base22.com>
2017-05-11 10:22:09 -0500

    Move Configuration components into ConfigurationModule

--- src/app/root-content/root-content.module.ts
+++ src/app/root-content/root-content.module.ts
@@ -11,26 +11,15 @@ import { EditInstanceComponent } from "./edit-instance/edit-instance.component";
 import { EditInstanceView } from "./edit-instance/edit-instance.view";
 import { SPARQLClientView } from "./sparql-client/sparql-client.view";
 // import { ExplorerView } from "./explorer/explorer.view";
-// Components -> Configuration
-import { BackupExporterComponent } from "./configuration/backup/backup-exporter/backup-exporter.component";
-import { BackupImporterComponent } from "./configuration/backup/backup-importer/backup-importer.component";
-import { BackupsListComponent } from "./configuration/backup/backups-list/backups-list.component";
-import { BackupsComponent } from "./configuration/backup/backups.component";
-import { ConfigurationComponent } from "./configuration/configuration.component";
-import { ConfigurationView } from "./configuration/configuration.view";
-
-//Directives
-import { BackupFileValidator, AtLeastOneValidValidator } from "./configuration/backup/backup-importer/backup-importer-validators";
 
 // Modules
 import { SharedModule } from "app/shared/shared.module";
 import { SPARQLClientModule } from "./sparql-client/sparql-client.module";
+import { ConfigurationModule } from "./configuration/configuration.module";
 // import { DocumentExplorerModule } from "./explorer/document-explorer/document-explorer.module";
 
 // Services
 // import { DocumentsResolverService } from "./explorer/document-explorer/documents-resolver.service";
-import { JobsService } from "./configuration/job/jobs.service";
-import { BackupsService } from "./configuration/backup/backups.service";
 
 
 @NgModule( {
@@ -40,6 +29,7 @@ import { BackupsService } from "./configuration/backup/backups.service";
 		routing,
 		SharedModule,
 		SPARQLClientModule,
+		ConfigurationModule,
 		// DocumentExplorerModule,
 	],
 	declarations: [
@@ -49,20 +39,10 @@ import { BackupsService } from "./configuration/backup/backups.service";
 		EditInstanceComponent,
 		EditInstanceView,
 		// ExplorerView,
-		BackupExporterComponent,
-		BackupImporterComponent,
-		BackupsListComponent,
-		BackupsComponent,
-		ConfigurationComponent,
-		ConfigurationView,
-		BackupFileValidator,
-		AtLeastOneValidValidator
 	],
 	exports: [],
 	providers: [
 		// DocumentsResolverService,
-		JobsService,
-		BackupsService,
 	],
 } )
 export class AppContentModule {
