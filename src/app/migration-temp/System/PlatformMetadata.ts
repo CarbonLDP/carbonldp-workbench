d441aa321a2f2c3025ed4c7d110575f3dfcbec85
Alvaro Contreras <alvaro.contreras@base22.com>
2017-05-18 15:22:09 -0500

    Delete all migration-temp files

--- src/app/migration-temp/System/PlatformMetadata.ts
+++
@@ -1,9 +0,0 @@
-import * as VolatileResource from "carbonldp/LDP/VolatileResource";
-import * as ObjectSchema from "carbonldp/ObjectSchema";
-export declare const RDF_CLASS:string;
-export declare const SCHEMA:ObjectSchema.Class;
-export interface Class extends VolatileResource.Class {
-	version:string;
-	buildDate:Date;
-}
-export default Class;
