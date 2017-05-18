4cd8daebef48587ef3b4182c643c7cffea9f019c
Alvaro Contreras <alvaro.contreras@base22.com>
2017-05-18 15:44:36 -0500

    Change getAPIDescription calls to getPlatformMetadata

--- src/app/versions-presenter/versions-presenter.component.ts
+++ src/app/versions-presenter/versions-presenter.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef } from "@angular/core";
 import { Class as Carbon } from "carbonldp/Carbon";
-import * as APIDescription from "carbonldp/APIDescription";
+import { Class as PlatformMetadata } from "carbonldp/System/PlatformMetadata";
 import * as $ from "jquery";
 
 @Component( {
@@ -25,16 +25,15 @@ export class VersionsPresenterComponent {
 
 	ngOnInit():void {
 		this.$element = $( this.element.nativeElement );
-		this.carbonldpSDK = "v." + this.carbon.version;
-		this.carbonldpWorkbench = "v." + process.env.PACKAGES[ "carbonldp-workbench" ];
-		this.carbonldpURL = this.carbon.getSetting( "http.ssl" ) ? "https" : "http";
-		this.carbonldpURL += "://" + this.carbon.getSetting( "domain" );
+		this.carbonldpSDK = "v" + this.carbon.version;
+		this.carbonldpWorkbench = "v" + process.env.PACKAGES[ "carbonldp-workbench" ];
+		this.carbonldpURL = this.carbon.baseURI;
 		this.getApiVersion();
 	}
 
 	getApiVersion():void {
-		this.carbon.getAPIDescription().then( ( apiDescription:APIDescription.Class ) => {
-			this.carbonldpPlatform = "v." + apiDescription.version;
+		this.carbon.getPlatformMetadata().then( ( platformMetadata:PlatformMetadata ) => {
+			this.carbonldpPlatform = "v" + platformMetadata.version;
 		} ).catch( ( error ) => {
 			this.$element.find( "span.platform" ).popup( { hideOnScroll: false } ).popup( "show" );
 		} );
