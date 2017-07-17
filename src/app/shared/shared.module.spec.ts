e39b88601b7d48167f3dddbc3c9b38b45068355f
Alvaro Contreras <alvaro.contreras@base22.com>
2017-07-17 12:37:54 -0500

    Add tests for messageAreaService

--- src/app/shared/shared.module.spec.ts
+++ src/app/shared/shared.module.spec.ts
@@ -2,6 +2,7 @@ import { directivesSpecs } from "./directives/directives.module.spec";
 import { errorLabelSpecs } from "./messages-area/error/error-label.component.spec";
 import { errorMessageGeneratorSpecs } from "./messages-area/error/error-message-generator.spec";
 import { messageComponentSpecs } from "./messages-area/message.component.spec";
+import { messageAreaServiceSpecs } from "./messages-area/messages-area.service.spec";
 
 describe( "SharedModule", () => {
 
@@ -13,4 +14,5 @@ describe( "SharedModule", () => {
 
 	messageComponentSpecs();
 
+	messageAreaServiceSpecs();
 } );
