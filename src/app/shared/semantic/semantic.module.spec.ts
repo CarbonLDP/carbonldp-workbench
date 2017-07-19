cda3da4afef0c8b13da2c346940b394b86397936
Alvaro Contreras <alvaro.contreras@base22.com>
2017-07-19 16:31:39 -0500

    Add tests for TabComponent

--- src/app/shared/semantic/semantic.module.spec.ts
+++ src/app/shared/semantic/semantic.module.spec.ts
@@ -1,7 +1,10 @@
 import { collapsibleSpecs } from "./collapsible.directive.spec";
+import { tabComponentSpecs } from "./tab.component.spec";
 
 describe( "SemanticModule", () => {
 
 	collapsibleSpecs();
+
+	tabComponentSpecs();
 
 } );
