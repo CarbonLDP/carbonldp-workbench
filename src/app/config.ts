8c38aa402d1ece4ecaa8e56490f8e7b66c7a239d
Alvaro Contreras <alvaro.contreras@base22.com>
2017-08-03 16:21:25 -0500

    Update minor packages and remove unused oens

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,5 @@
-export const CARBON_PROTOCOL:string = globalConfigIsSet( "CARBON_PROTOCOL" ) ? window[ "__CONFIG" ][ "CARBON_PROTOCOL" ] : process.env.CARBON.protocol;
-export const CARBON_HOST:string = globalConfigIsSet( "CARBON_HOST" ) ? window[ "__CONFIG" ][ "CARBON_HOST" ] : process.env.CARBON.domain;
+export const CARBON_PROTOCOL:string = globalConfigIsSet( "CARBON_PROTOCOL" ) ? window[ "__CONFIG" ][ "CARBON_PROTOCOL" ] : (<any>process.env.CARBON).protocol;
+export const CARBON_HOST:string = globalConfigIsSet( "CARBON_HOST" ) ? window[ "__CONFIG" ][ "CARBON_HOST" ] : (<any>process.env.CARBON).domain;
 export const DEBUG:boolean = (globalConfigIsSet( "DEBUG" ) ? window[ "__CONFIG" ][ "ENV" ] : process.env.ENV) !== "production";
 export function BASE_URL():string {
 	return globalConfigIsSet( "BASE_URL" ) ? window[ "__CONFIG" ][ "BASE_URL" ] : process.env.baseUrl
