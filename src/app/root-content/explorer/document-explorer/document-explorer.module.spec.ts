a4e5cad9adca9177e5656d2d6de935d5a00ccfbd
Alvaro Contreras <alvaro.contreras@base22.com>
2017-08-18 11:12:47 -0500

    Add tests for PointerComponent

--- src/app/root-content/explorer/document-explorer/document-explorer.module.spec.ts
+++ src/app/root-content/explorer/document-explorer/document-explorer.module.spec.ts
@@ -1,9 +1,16 @@
 import { literalSpecs } from "./literals/literal.component.spec";
 import { literalsSpecs } from "./literals/literals.component.spec";
+import { pointerSpecs } from "./pointers/pointer.component.spec";
 
 describe( "SharedModule", () => {
 
-	literalSpecs();
+	describe( "Literals", () => {
+		literalSpecs();
 
-	literalsSpecs();
+		literalsSpecs();
+	} );
+
+	describe( "Pointers", () => {
+		pointerSpecs();
+	} );
 } );
