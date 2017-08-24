a1111e7fb12b8e013d295645f3f37b364be6e0c2
Alvaro Contreras <alvaro.contreras@base22.com>
2017-08-24 12:44:36 -0500

    Add tests for ListsComponent

--- src/app/root-content/explorer/document-explorer/document-explorer.module.spec.ts
+++ src/app/root-content/explorer/document-explorer/document-explorer.module.spec.ts
@@ -3,6 +3,7 @@ import { literalsSpecs } from "./literals/literals.component.spec";
 import { pointerSpecs } from "./pointers/pointer.component.spec";
 import { pointersSpecs } from "./pointers/pointers.component.spec";
 import { listSpecs } from "./lists/list.component.spec";
+import { listsSpecs } from "./lists/lists.component.spec";
 
 describe( "SharedModule", () => {
 
@@ -20,5 +21,7 @@ describe( "SharedModule", () => {
 
 	describe( "Lists", () => {
 		listSpecs();
+
+		listsSpecs();
 	} );
 } );
