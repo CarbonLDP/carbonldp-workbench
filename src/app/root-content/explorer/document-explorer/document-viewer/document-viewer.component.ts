5b6c9da4961fa6981eff982764891558bfe55ff5
Alvaro Contreras <alvaro.contreras@base22.com>
2017-08-29 16:25:37 -0500

    Remove bNodeIdentifier while retrieving blank nodes

--- src/app/root-content/explorer/document-explorer/document-viewer/document-viewer.component.ts
+++ src/app/root-content/explorer/document-explorer/document-viewer/document-viewer.component.ts
@@ -140,7 +140,6 @@ export class DocumentViewerComponent implements AfterViewInit, OnChanges {
 			( bNode:RDFNode.Class ) => {
 				return {
 					id: bNode[ "@id" ],
-					bNodeIdentifier: bNode[ "https://carbonldp.com/ns/v1/platform#bNodeIdentifier" ][ 0 ][ "@value" ],
 					copy: bNode
 				}
 			} );
