5809004195017475f9f396e46ff1bde34d2fea0a
Alvaro Contreras <alvaro.contreras@base22.com>
2018-02-14 13:03:22 -0600

    Add Guards into app/angular-carbonldp

---
+++ src/app/angular-carbonldp/guards/abstract-authentication.guard.ts
@@ -0,0 +1,43 @@
+import { Injectable } from "@angular/core";
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from "@angular/router";
+
+import { inject, carbonProvider } from "./../utils";
+import { AuthService } from "./../services";
+
+@Injectable()
+export abstract class AbstractAuthenticationGuard implements CanActivate {
+	protected authService:AuthService.Class;
+
+	constructor( protected router:Router ) {}
+
+	canActivate( route:ActivatedRouteSnapshot, state:RouterStateSnapshot ):Promise<boolean> {
+		return carbonProvider.promise.then( () => {
+			// AuthService needs to be injected here so we don't cause a premature initialization of AuthService
+			// If AuthService is injected in the constructor, the carbonProvider won't be ready and will cause an error
+			return inject( AuthService.Token );
+		} ).then( ( authService:any ) => {
+			this.authService = authService;
+			return true;
+		} ).catch( ( error ) => {
+			return this.onError( route, state );
+		} );
+	}
+
+	protected onReject( route:ActivatedRouteSnapshot, state:RouterStateSnapshot ):boolean {
+		if( typeof route.data === "object" && route.data !== null && typeof route.data[ "onReject" ] !== "undefined" ) {
+			this.router.navigate( route.data[ "onReject" ] );
+		} else {
+			console.error( "AuthenticatedGuard was configured in a route without an 'onReject' property" );
+		}
+		return false;
+	}
+
+	protected onError( route:ActivatedRouteSnapshot, state:RouterStateSnapshot ):boolean {
+		if( typeof route.data === "object" && route.data !== null && typeof route.data[ "onError" ] !== "undefined" ) {
+			this.router.navigate( route.data[ "onError" ] );
+		} else {
+			return this.onReject( route, state );
+		}
+		return false;
+	}
+}
