a3d7b275d7a7de1c84ca1818fce446d78ad481d0
Alvaro Contreras <alvaro.contreras@base22.com>
2018-02-14 16:35:40 -0600

    Change signature of createUser function's returning parameters

--- src/app/angular-carbonldp/services/carbon-auth.service.ts
+++ src/app/angular-carbonldp/services/carbon-auth.service.ts
@@ -58,7 +58,7 @@ export class CarbonAuthService implements AuthService.Class {
 	register( name:string, username:string, password:string ):Promise<any>;
 	register( name:string, username:string, password:string, enabled:boolean ):Promise<any>;
 	register( name:string, username:string, password:string, enabled?:boolean ):Promise<any> {
-		return this.carbon.auth.users.register( username, password, enabled ).then( ( [ persistedUser, responses ]:[ PersistedUser.Class, HTTP.Response.Class[] ] ) => {
+		return this.carbon.auth.users.register( username, password, enabled ).then( ( [ persistedUser, responses ]:[ PersistedUser.Class, HTTP.Response.Class ] ) => {
 			persistedUser.name = name;
 			return persistedUser.saveAndRefresh();
 		} );
