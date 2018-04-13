1a1a8a43e8ebb9656603074cd789d70d68101464
Alvaro Contreras <alvaro9210@gmail.com>
2018-04-13 18:09:06 -0500

    Remove rules that already exist in webpack.common

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,96 +1,49 @@
 const commonConfig = require( "./webpack.common.js" );
 const helpers = require( "./webpack.helpers" );
+const webpackMerge = require( "webpack-merge" );
 const config = require( "./dev.config.json" );
 const carbonConfig = config.carbon;
-const webpackMerge = require( "webpack-merge" );
-
-// carbonldp's projects versions
-const workbench = require( "../package.json" );
-
-
-// Plugins
-const DefinePlugin = require( "webpack/lib/DefinePlugin" );
-const ContextReplacementPlugin = require( "webpack/lib/ContextReplacementPlugin" );
-const HtmlWebpackPlugin = require( "html-webpack-plugin" );
-const AngularCompilerPlugin = require( "@ngtools/webpack" ).AngularCompilerPlugin;
+const devServerConfig = config[ "webpack-dev-server" ];
 
 
 // Webpack Constants
-const ENV = process.env.ENV = process.env.NODE_ENV = "development";
-const PROTOCOL = process.env.PROTOCOL || "http";
-const HOST = process.env.HOST || "localhost";
-const PORT = process.env.PORT || 8080;
 const METADATA = {
 	baseUrl: config.url.base,
-	protocol: PROTOCOL,
-	host: HOST,
-	port: PORT,
-	ENV: ENV,
-	isDevServer: helpers.isWebpackDevServer(),
+	protocol: devServerConfig.protocol,
+	host: devServerConfig.host,
+	port: devServerConfig.port,
+	ENV: "development",
+	isDevServer: true,
 	CARBON: {
 		protocol: carbonConfig.protocol,
 		domain: carbonConfig.domain,
 	}
 };
 
-module.exports = webpackMerge( commonConfig, {
-		devtool: "source-map",
 
-		mode: "development",
-		// TODO: Use [ "awesome-typescript-loader", "angular2-template-loader", "angular-router-loader" ] when ATL provides compatibility with Webpack 4
+module.exports = webpackMerge( commonConfig( METADATA ), {
+	devtool: "source-map",
 
-		output: {
-			path: helpers.root( "dist" ),
-			filename: "[name].js",
-			sourceMapFilename: "[file].map",
-			chunkFilename: "[id].chunk.js",
-		},
+	mode: "development",
+	// Another rule to load ts files is to use [ "awesome-typescript-loader", "angular2-template-loader", "angular-router-loader" ]
 
-		plugins: [
+	output: {
+		path: helpers.root( "dist" ),
+		filename: "[name].js",
+		sourceMapFilename: "[file].map",
+		chunkFilename: "[id].chunk.js",
+	},
 
-			// Workaround for angular/angular#11580
-			new ContextReplacementPlugin(
-				// The (\\|\/) piece accounts for path separators in *nix and Windows
-				/\@angular(\\|\/)core(\\|\/)esm5/,
-				helpers.root( "./src" ), // location of your src
-				{} // a map of your routes
-			),
+	devServer: {
+		open: true,	                // Opens web browser
+		port: METADATA.port,	    // Port of project
+		host: METADATA.host,        // Host to use by the dev server
+		inline: true,	            // A script will be inserted in index.html to take care of live reloading, and build messages will appear in the browser console
+		historyApiFallback: true,	// Server index.html page when 404 responses
+		watchOptions: {
+			aggregateTimeout: 300,	// Add a delay in milliseconds before rebuilding
+			poll: 1000	            // Check for changes every second
+		}
+	},
 
-			new DefinePlugin( {
-				"process.env": {
-					"baseUrl": JSON.stringify( METADATA.baseUrl ),
-					"ENV": JSON.stringify( METADATA.ENV ),
-					"CARBON": {
-						"protocol": JSON.stringify( carbonConfig.protocol ),
-						"domain": JSON.stringify( carbonConfig.domain ),
-					},
-					"PACKAGES": {
-						"carbonldp-workbench": JSON.stringify( workbench.version ),
-					}
-				}
-			} ),
-
-
-			// Webpack inject scripts and links for us with the HtmlWebpackPlugin and also
-			// defines the order of the chunks: polyfills -> vendor -> app
-			new HtmlWebpackPlugin( {
-				template: "./src/index.html",
-				chunks: [ "polyfills", "vendor", "styles", "app" ],
-				chunksSortMode: "manual",
-				metadata: METADATA
-			} )
-		],
-
-		devServer: {
-			open: true,	                // Opens web browser
-			port: METADATA.port,	    // Port of project
-			host: METADATA.host,        // Host to use by the dev server
-			inline: true,	            // A script will be inserted in index.html to take care of live reloading, and build messages will appear in the browser console
-			historyApiFallback: true,	// Server index.html page when 404 responses
-			watchOptions: {
-				aggregateTimeout: 300,	// Add a delay in milliseconds before rebuilding
-				poll: 1000	            // Check for changes every second
-			}
-		},
-
-	} );
+} );
