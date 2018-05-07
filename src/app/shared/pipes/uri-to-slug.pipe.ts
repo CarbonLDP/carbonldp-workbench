5c79d6c52819897fc04ae9a1478338406710a7aa
Alvaro Contreras <alvaro9210@gmail.com>
2018-05-07 13:42:06 -0500

    Change URIToSlugPipe to return value even if it's empty

--- src/app/shared/pipes/uri-to-slug.pipe.ts
+++ src/app/shared/pipes/uri-to-slug.pipe.ts
@@ -6,9 +6,6 @@ import { URI } from "carbonldp/RDF/URI";
 export class URIToSlugPipe implements PipeTransform {
 	transform( uri:string ):string {
 
-		if( ! uri ) throw new Error( "The URI-to-slug pipe requires a string" );
-		if( ! URI.isAbsolute( uri ) ) throw new Error( "The URI-to-slug pipe requires an absolute URI" );
-
-		return URI.getSlug( uri );
+		return ! uri ? uri : URI.getSlug( uri );
 	}
 }
