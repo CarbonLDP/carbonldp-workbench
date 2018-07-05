d16b319b181a5aef9b7d1500827b719f18ac5833
Alvaro Contreras <alvaro9210@gmail.com>
2018-07-05 17:00:23 -0500

    Refactor and clean code of WidgetsService

--- src/app/workbench/dashboard/widgets/widgets.service.ts
+++ src/app/workbench/dashboard/widgets/widgets.service.ts
@@ -3,7 +3,11 @@ import { Injectable } from "@angular/core";
 import { CarbonLDP } from "carbonldp";
 import { PlatformMetadata } from "carbonldp/System/PlatformMetadata";
 import { SPARQLSelectResults } from "carbonldp/SPARQL/SelectResults";
+import { C } from "carbonldp/Vocabularies";
 
+/*
+*  Services used by the widgets
+* */
 @Injectable()
 export class WidgetsService {
 	carbonldp:CarbonLDP;
@@ -12,48 +16,61 @@ export class WidgetsService {
 		this.carbonldp = carbonldp;
 	}
 
+	/*
+	*  Returns the total number of documents existing in the platform
+	* */
 	getDocumentsTotalCount():Promise<number> {
 		let count;
 		let query:string = `
 			SELECT ( COUNT (?document) as ?count)
 			WHERE{ GRAPH ?document {
-					?document a <https://carbonldp.com/ns/v1/platform#Document>
+					?document a <${C.Document}>
 				}
 			}
 		`;
 
-		return this.carbonldp.documents.executeSELECTQuery( '', query ).then( ( results:SPARQLSelectResults ) => {
-			results.bindings.forEach( ( binding ) => {
-				count = binding[ "count" ];
+		return this.carbonldp.documents.executeSELECTQuery( '', query )
+			.then( ( results:SPARQLSelectResults ) => {
+				results.bindings.forEach( ( binding ) => {
+					count = binding[ "count" ];
+				} );
+				return count;
 			} );
-			return count;
-		} );
-
 	}
 
+
+	/*
+	*  Returns the total number of triples existing in the platform
+	* */
 	getTriplesTotalCount():Promise<number> {
 		let count;
 		let query:string = `
-			PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
 			SELECT ( COUNT (?s) as ?count)
 			WHERE{ 
 				?s ?p ?o .
 			}
 		`;
 
-		return this.carbonldp.documents.executeSELECTQuery( '', query ).then( ( results:SPARQLSelectResults ) => {
-			results.bindings.forEach( ( binding ) => {
-				count = binding[ "count" ];
+		return this.carbonldp.documents.executeSELECTQuery( '', query )
+			.then( ( results:SPARQLSelectResults ) => {
+				results.bindings.forEach( ( binding ) => {
+					count = binding[ "count" ];
+				} );
+				return count;
 			} );
-			return count;
-		} );
 
 	}
 
-	getPlatformMetadata():Promise<any> {
+	/*
+	*  Returns the Metadata of the platform
+	* */
+	getPlatformMetadata():Promise<PlatformMetadata> {
 		return this.carbonldp.getPlatformMetadata();
 	}
 
+	/*
+	*  Refresh the metadata of the platform
+	* */
 	refreshPlatformMetadata( platformMetadata:PlatformMetadata ):Promise<any> {
 		return platformMetadata.refresh();
 	}
