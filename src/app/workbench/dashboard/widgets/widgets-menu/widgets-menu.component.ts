498aa9d9032ad0d0bd7f27b7fd36f97df931cf4e
Alvaro Contreras <alvaro9210@gmail.com>
2018-07-05 16:58:57 -0500

    Refactor and clean code of WidgetsMenuComponent

--- src/app/workbench/dashboard/widgets/widgets-menu/widgets-menu.component.ts
+++ src/app/workbench/dashboard/widgets/widgets-menu/widgets-menu.component.ts
@@ -1,32 +1,38 @@
-import { Component, Input } from "@angular/core";
-
-import { Widget } from "../widgets.component";
+import { Component, Input, ElementRef } from "@angular/core";
 
 import * as $ from "jquery";
 
 import { WidgetsService } from "../widgets.service";
+import { Widget } from "../widgets.component";
 
+
+/*
+*  Menu that lists all the Dashboard's Widgets
+* */
 @Component( {
 	selector: "cw-widgets-menu",
 	templateUrl: "./widgets-menu.component.html",
 	styleUrls: [ "./widgets-menu.component.scss" ],
 } )
-
 export class WidgetsMenu {
+	$element:JQuery;
+	element:ElementRef;
 	widgetsService:WidgetsService;
 
 	@Input() widgetsList:Widget[];
 
-	constructor( widgetsService:WidgetsService ) {
+	constructor( element:ElementRef, widgetsService:WidgetsService ) {
+		this.element = element;
 		this.widgetsService = widgetsService;
 	}
 
-	ngOnInit():void {
+	ngAfterViewInit():void {
+		this.$element = $( this.element.nativeElement );
 		this.initializeDropdown();
 	}
 
 	initializeDropdown() {
-		$( ".ui.dropdown" ).dropdown( {
+		this.$element.find( ".ui.dropdown" ).dropdown( {
 			action: "nothing"
 		} );
 	}
