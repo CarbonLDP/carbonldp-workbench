43624a5126d7d24f40f70ce2d2c9a3cbc8440fe4
Alvaro Contreras <alvaro9210@gmail.com>
2018-07-05 16:57:53 -0500

    Refactor and clean code of InstanceWidgetComponent

--- src/app/workbench/dashboard/widgets/instance/instance-widget.component.ts
+++ src/app/workbench/dashboard/widgets/instance/instance-widget.component.ts
@@ -1,21 +1,21 @@
-import { Component, ElementRef, Output, EventEmitter } from "@angular/core";
+import { Component, Output, HostBinding, ElementRef, EventEmitter } from "@angular/core";
 
 import { CarbonLDP } from "carbonldp";
-import { C } from "carbonldp/Vocabularies";
 import { PlatformMetadata } from "carbonldp/System/PlatformMetadata";
 
 import { WidgetsService } from "../widgets.service";
-import { Message } from "app/shared/messages-area/message.component";
 import { ErrorMessageGenerator } from "app/shared/messages-area/error/error-message-generator";
 
+
+/*
+*  Widget that displays specific data of the Carbon LDP instance
+* */
 @Component( {
 	selector: "cw-instance-widget",
 	templateUrl: "./instance-widget.component.html",
 	styleUrls: [ "./instance-widget.component.scss" ],
 } )
-
 export class InstanceWidgetComponent {
-	private element:ElementRef;
 	private carbonldp:CarbonLDP;
 	private widgetsService:WidgetsService;
 
@@ -25,12 +25,10 @@ export class InstanceWidgetComponent {
 	hide:boolean = false;
 	platformMetadata:PlatformMetadata;
 
-	errorMessage:Message;
-
 	@Output() onErrorOccurs:EventEmitter<any> = new EventEmitter();
+	@HostBinding( "class.error" ) hasError:boolean = false;
 
-	constructor( element:ElementRef, carbonldp:CarbonLDP, widgetsService:WidgetsService ) {
-		this.element = element;
+	constructor( carbonldp:CarbonLDP, widgetsService:WidgetsService ) {
 		this.carbonldp = carbonldp;
 		this.widgetsService = widgetsService;
 	}
@@ -39,7 +37,7 @@ export class InstanceWidgetComponent {
 		this.getPlatformMetadata();
 	}
 
-	public collapseWidget( $event:MouseEvent ):void {
+	collapseWidget( $event:MouseEvent ):void {
 		$event.stopImmediatePropagation();
 		this.hide = ! this.hide;
 		if( ! this.hide ) {
@@ -47,59 +45,46 @@ export class InstanceWidgetComponent {
 		}
 	}
 
-	public refreshWidget( event?:MouseEvent ):Promise<any> {
-		if( event ) event.stopImmediatePropagation();
-		this.errorMessage = null;
-		this.carbonldpBuildDate = null;
+	resetWidget():void {
+		this.hasError = false;
 		this.carbonldpVersion = null;
+		this.carbonldpBuildDate = null;
+	}
 
-		if( ! this.platformMetadata ) return this.getPlatformMetadata();
+	setValues( version:string, buildDate:Date ):void {
+		this.carbonldpURL = this.carbonldp.baseURI;
+		this.carbonldpVersion = version;
+		this.carbonldpBuildDate = buildDate;
+	}
 
+	refreshWidget( event?:MouseEvent ):Promise<any> {
+		if( event ) event.stopImmediatePropagation();
+		this.resetWidget();
 
-		return this.widgetsService.refreshPlatformMetadata( this.platformMetadata ).then( ( platformMetadata:any ) => {
-			this.platformMetadata = platformMetadata;
-			this.carbonldpBuildDate = platformMetadata.instance.buildDate;
-			this.carbonldpVersion = platformMetadata.instance.version;
-			this.element.nativeElement.classList.remove( "error" );
-		} ).catch( ( error:any ) => {
-			this.errorWidget( error );
-		} );
+		return ! this.platformMetadata ? this.getPlatformMetadata() : this.refreshPlatformMetadata();
+	}
+
+	private refreshPlatformMetadata():Promise<any> {
+		this.resetWidget();
+		return this.widgetsService.refreshPlatformMetadata( this.platformMetadata )
+			.then( ( platformMetadata:PlatformMetadata ) => {
+				this.platformMetadata = platformMetadata;
+				this.setValues( platformMetadata.instance.version, platformMetadata.instance.buildDate )
+			} ).catch( ( error:any ) => {
+				this.hasError = true;
+				this.onErrorOccurs.emit( ErrorMessageGenerator.getErrorMessage( error ) );
+			} );
 	}
 
 	private getPlatformMetadata():Promise<any> {
-		// TODO: Remove extendObjectSchema when SDK implements instance with its properties
-		this.carbonldp.extendObjectSchema( C.namespace + "PlatformInstance", {
-			"version": {
-				"@id": C.namespace + "version",
-				"@type": "string"
-			},
-			"buildDate": {
-				"@id": C.namespace + "buildDate",
-				"@type": "dateTime"
-			}
-		} );
-		this.carbonldp.extendObjectSchema( C.namespace + "Platform", {
-			"instance": {
-				"@id": C.namespace + "instance",
-				"@type": "@id"
-			}
-		} );
-
-		return this.widgetsService.getPlatformMetadata().then( ( platformMetadata:any ) => {
-			this.platformMetadata = platformMetadata;
-			this.carbonldpURL = this.carbonldp.baseURI;
-			this.carbonldpBuildDate = platformMetadata.instance.buildDate;
-			this.carbonldpVersion = platformMetadata.instance.version;
-			this.element.nativeElement.classList.remove( "error" );
-		} ).catch( ( error:any ) => {
-			this.errorWidget( error );
-		} );
-	}
-
-
-	private errorWidget( error ) {
-		this.element.nativeElement.classList.add( "error" );
-		this.errorMessage = ErrorMessageGenerator.getErrorMessage( error );
-		this.onErrorOccurs.emit( this.errorMessage );
+		this.resetWidget();
+		return this.widgetsService.getPlatformMetadata()
+			.then( ( platformMetadata:PlatformMetadata ) => {
+				this.platformMetadata = platformMetadata;
+				this.setValues( platformMetadata.instance.version, platformMetadata.instance.buildDate )
+			} ).catch( ( error:any ) => {
+				this.hasError = true;
+				this.onErrorOccurs.emit( ErrorMessageGenerator.getErrorMessage( error ) );
+			} );
 	}
 }
