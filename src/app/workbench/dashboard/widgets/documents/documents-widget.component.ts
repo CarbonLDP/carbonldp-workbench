851be6687a3f8bed79f25d3c7240d0bd29c6547d
Alvaro Contreras <alvaro9210@gmail.com>
2018-07-05 16:57:38 -0500

    Refactor and clean code of DocumentsWidgetComponent

--- src/app/workbench/dashboard/widgets/documents/documents-widget.component.ts
+++ src/app/workbench/dashboard/widgets/documents/documents-widget.component.ts
@@ -1,32 +1,33 @@
-import { Component, ElementRef, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output, ElementRef, HostBinding, EventEmitter } from "@angular/core";
 
 import { CarbonLDP } from "carbonldp";
 
 import { Widget } from "../widgets.component";
 import { WidgetsService } from "../widgets.service";
-import { Message } from "app/shared/messages-area/message.component";
 import { ErrorMessageGenerator } from "app/shared/messages-area/error/error-message-generator";
 
-import "semantic-ui/semantic";
 
+/*
+*  Widget that displays the total number of Document in a Carbon LDP instance
+* */
 @Component( {
 	selector: "cw-documents-widget",
 	templateUrl: "./documents-widget.component.html",
 	styleUrls: [ "./documents-widget.component.scss" ]
 } )
-
 export class DocumentsWidgetComponent {
-	carbonldp:CarbonLDP;
-	element:ElementRef;
-	errorMessage:Message;
-	widgetsService:WidgetsService;
-	documentsTotalCount;
+	private element:ElementRef;
+	private carbonldp:CarbonLDP;
+	private widgetsService:WidgetsService;
+
+	documentsTotalCount:number;
 
 	private oldWidgetHide:boolean;
 
 	@Input() widget:Widget;
 	@Output() onErrorOccurs:EventEmitter<any> = new EventEmitter();
 	@Output() onClose:EventEmitter<Widget> = new EventEmitter<Widget>();
+	@HostBinding( "class.error" ) hasError:boolean = false;
 
 	constructor( element:ElementRef, carbonldp:CarbonLDP, widgetsService:WidgetsService ) {
 		this.element = element;
@@ -46,35 +47,29 @@ export class DocumentsWidgetComponent {
 		this.getDocumentsCount();
 	}
 
-	public refreshWidget() {
-		this.errorMessage = null;
-		this.documentsTotalCount = null;
+	refreshWidget() {
+		this.resetWidget();
 		this.getDocumentsCount();
 	}
 
-	public closeWidget() {
+	closeWidget() {
 		this.onClose.emit( this.widget );
 	}
 
-	public errorWidget( error ) {
-		this.element.nativeElement.classList.add( "error" );
-		this.errorMessage = this.getErrorMessage( error );
-		this.onErrorOccurs.emit( this.errorMessage );
+	private resetWidget():void {
+		this.hasError = false;
+		this.documentsTotalCount = null;
 	}
 
-	public getErrorMessage( error:any ):Message {
-		return ErrorMessageGenerator.getErrorMessage( error );
-	}
-
-	public getDocumentsCount() {
+	private getDocumentsCount() {
+		this.resetWidget();
 		this.widgetsService.getDocumentsTotalCount()
 			.then( ( count ) => {
-				let widget = document.querySelector( ".widget-container--totalDocuments" );
 				this.documentsTotalCount = count;
-				widget.classList.remove( "error" );
 			} )
 			.catch( ( error:any ) => {
-				this.errorWidget( error );
+				this.hasError = true;
+				this.onErrorOccurs.emit( ErrorMessageGenerator.getErrorMessage( error ) );
 			} );
 	}
 }
