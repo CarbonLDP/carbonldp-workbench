3b715c76ff77e1066c43ddd25f320b0408814a9e
Alvaro Contreras <alvaro9210@gmail.com>
2018-07-23 10:49:13 -0500

    Remove unused link of head/config file

--- config/head.config.js
+++ config/head.config.js
@@ -1,7 +1,5 @@
 // Configuration for head elements added during the creation of index.html.
 module.exports = {
-	link: [
-		{ rel: "stylesheet", href: "semantic/dist/semantic.min.css" }
-	],
+	link: [],
 	meta: []
 };
