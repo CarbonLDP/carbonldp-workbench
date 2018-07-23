3cdbd99cbf684a3665747f82c1850b0a3b9136df
Alvaro Contreras <alvaro9210@gmail.com>
2018-07-23 10:45:23 -0500

    Add PropertyTypeComponent to display properties with @type

---
+++ src/app/workbench/explorer/document-explorer/property/property-type/property-type.component.ts
@@ -0,0 +1,53 @@
+import { Component, Input } from "@angular/core";
+
+import { URI } from "carbonldp/RDF/URI";
+import { LDP } from "carbonldp/Vocabularies/LDP";
+
+import { PropertyRow } from "../property.component";
+
+@Component( {
+	selector: "cw-property-type",
+	templateUrl: "./property-type.component.html",
+	// styleUrls: [ "./property-type.component.scss" ],
+} )
+
+export class PropertyTypeComponent {
+
+	types:string[] = [];
+
+
+	private _property:PropertyRow;
+	@Input()
+	set property( prop:PropertyRow ) {
+		this.types = prop.copy.value;
+	}
+
+	get property():PropertyRow { return this._property; }
+
+
+	getDisplayName( uri:string ):string {
+		return this.unescape( this.getFragment( uri ) );
+	}
+
+	getFragment( uri:string ):string {
+		return URI.getFragment( uri );
+	}
+
+	getTypeIcon( type:string ):string {
+		switch( type ) {
+			case LDP.RDFSource:
+				return "file outline";
+			case LDP.Container:
+				return "cubes";
+			case LDP.BasicContainer:
+				return "cube";
+			default:
+				return "file excel outline";
+		}
+	}
+
+	private unescape( uri:string ):string {
+		return decodeURI( uri );
+	}
+}
+
