a962f284a99700bf186586803af5e8ff7749c356
Alvaro Contreras <alvaro9210@gmail.com>
2018-07-24 02:48:18 -0500

    Simplify code of Property content component

--- src/app/workbench/explorer/document-explorer/property/property-content/property-content.component.ts
+++ src/app/workbench/explorer/document-explorer/property/property-content/property-content.component.ts
@@ -4,7 +4,6 @@ import { RDFLiteral } from "carbonldp/RDF/Literal";
 import { RDFList } from "carbonldp/RDF/List";
 import { URI } from "carbonldp/RDF/URI";
 import { RDFNode } from "carbonldp/RDF/Node"
-import { isArray } from "carbonldp/Utils";
 
 import { LiteralStatus } from "./../../literals/literal.component";
 import { PointerRow } from "./../../pointers/pointer.component";
@@ -25,23 +24,20 @@ export class PropertyContentComponent implements AfterViewInit, OnInit {
 	literals:LiteralStatus[] = [];
 	pointers:PointerRow[] = [];
 	lists:ListRow[] = [];
-	tempLiterals:LiteralStatus[];
-	tempPointers:PointerRow[];
-	tempLists:ListRow[];
+
+	// Variable that will have the JSON object of the property
+	// with all the changes made to the original property.
 	tempProperty:Property = <Property>{};
 	state:string;
-	existingFragments:string[] = [];
 
 	id:string;
-	originalId:string;
 	propertyName:string;
 	originalName:string;
-	value:any[] | string = [];
+	value:any[] = [];
 
 	addNewLiteral:EventEmitter<boolean> = new EventEmitter<boolean>();
 	addNewPointer:EventEmitter<boolean> = new EventEmitter<boolean>();
 	addNewList:EventEmitter<boolean> = new EventEmitter<boolean>();
-	commonToken:string[] = [ "@id", "@type", "@value" ];
 	modes:Modes = Modes;
 	@ViewChild( "nameInput" ) nameInputControl;
 
@@ -54,24 +50,22 @@ export class PropertyContentComponent implements AfterViewInit, OnInit {
 	@Input() existingProperties:string[] = [];
 	@Input() accessPointsHasMemberRelationProperties:string[] = [];
 	private _property:PropertyStatus;
-	@Input()
-	set property( prop:PropertyStatus ) {
+	@Input() set property( prop:PropertyStatus ) {
 		this._property = prop;
 		this.state = ! ! prop.copy ? (! ! prop.modified ? "modified" : "copy") : "added";
 
+		// Assign the id of the property
 		this.id = prop[ this.state ].id;
 		this.tempProperty.id = prop[ this.state ].id;
-		this.originalId = prop[ this.state ].value;
 
+		// Assign the name of the property
 		this.propertyName = prop[ this.state ].name;
 		this.tempProperty.name = prop[ this.state ].name;
 		this.originalName = this.propertyName;
-		if( isArray( prop[ this.state ].value ) ) {
-			this.value = [];
-			prop[ this.state ].value.forEach( ( literalOrRDFNode ) => { (<Array<any>>this.value).push( Object.assign( literalOrRDFNode ) ) } )
-		} else {
-			this.value = prop[ this.state ].value;
-		}
+
+		// Assign the value of the property
+		this.value = [];
+		prop[ this.state ].value.forEach( ( literalOrPointerOrList ) => { this.value.push( Object.assign( literalOrPointerOrList ) ) } )
 	}
 
 	get property():PropertyStatus { return this._property; }
@@ -85,15 +79,13 @@ export class PropertyContentComponent implements AfterViewInit, OnInit {
 
 	get nameHasChanged():boolean { return ! ! this.property.copy && this.property.copy.name !== this.tempProperty.name; };
 
-	get valueHasChanged():boolean { return ! ! this.property.copy && this.property.copy.value !== this.tempProperty.value; };
+	get literalsHaveChanged():boolean { return ! ! this.literals.find( ( literalStatus ) => {return ! ! literalStatus.modified || ! ! literalStatus.added || ! ! literalStatus.deleted; } ) };
 
-	get literalsHaveChanged():boolean { return ! ! this.tempLiterals.find( ( literalStatus ) => {return ! ! literalStatus.modified || ! ! literalStatus.added || ! ! literalStatus.deleted; } ) };
+	get pointersHaveChanged():boolean { return ! ! this.pointers.find( ( pointerStatus ) => {return ! ! pointerStatus.modified || ! ! pointerStatus.added || ! ! pointerStatus.deleted; } ) };
 
-	get pointersHaveChanged():boolean { return ! ! this.tempPointers.find( ( pointerStatus ) => {return ! ! pointerStatus.modified || ! ! pointerStatus.added || ! ! pointerStatus.deleted; } ) };
+	get listsHaveChanged():boolean { return ! ! this.lists.find( ( listStatus ) => {return ! ! listStatus.modified || ! ! listStatus.added || ! ! listStatus.deleted; } ) };
 
-	get listsHaveChanged():boolean { return ! ! this.tempLists.find( ( listStatus ) => {return ! ! listStatus.modified || ! ! listStatus.added || ! ! listStatus.deleted; } ) };
-
-	get propertyHasChanged():boolean { return this.nameHasChanged || this.valueHasChanged || this.literalsHaveChanged || this.pointersHaveChanged || this.listsHaveChanged; }
+	get propertyHasChanged():boolean { return this.nameHasChanged || this.literalsHaveChanged || this.pointersHaveChanged || this.listsHaveChanged; }
 
 	get isAccessPointHasMemberRelationProperty():boolean { return this.accessPointsHasMemberRelationProperties.indexOf( this.id ) !== - 1; }
 
@@ -104,7 +96,7 @@ export class PropertyContentComponent implements AfterViewInit, OnInit {
 	}
 
 	ngOnInit():void {
-		this.fillLiteralsAndPointers();
+		this.fillLiteralsPointersAndLists();
 	}
 
 	ngAfterViewInit():void {
@@ -114,43 +106,12 @@ export class PropertyContentComponent implements AfterViewInit, OnInit {
 		this.initializeDeletionDimmer();
 	}
 
-	private getSlug( uri:string ) {
-		return URI.getSlug( uri );
-	}
-
-	isUrl( uri:string ):boolean {
-		let r = /^(ftp|http|https):\/\/[^ "]+$/;
-		return r.test( uri );
-	}
-
 	goToBlankNode( id:string ):void {
 		this.onGoToBlankNode.emit( id );
 	}
 
 	goToNamedFragment( id:string ):void {
 		this.onGoToNamedFragment.emit( id );
-	}
-
-	private initializeAccordions():void {
-		this.$element.find( ".ui.accordion" ).accordion();
-	}
-
-	/*
-	*   Initializes the options dropdown for the property
-	*   allowing to edit its name or delete the property
-	* */
-	private initializePropertyButtons():void {
-		this.$element.find( ".ui.options.dropdown.button" ).dropdown( {
-			transition: "swing up"
-		} );
-		this.$element.find( ".ui.save-cancel.buttons .dropdown.button" ).dropdown();
-	}
-
-	/*
-	*   Initializes the delete property confirmation message
-	* */
-	private initializeDeletionDimmer():void {
-		this.$element.find( ".property.confirm-deletion.dimmer" ).dimmer( { closable: false } );
 	}
 
 	onEditName():void {
@@ -182,64 +143,24 @@ export class PropertyContentComponent implements AfterViewInit, OnInit {
 		this.mode = Modes.READ;
 	}
 
-	private sanitize( value:string ):string {
-		let sanitized:string = value;
-		let slug:string = this.getSlug( value );
-		let parts:string[] = value.split( slug );
-		if( parts.length > 0 ) sanitized = parts[ 0 ] + this.escape( slug );
-		return sanitized;
-	}
-
 	/*
 	*   Clean the variables that hold the literals, pointers and lists
-	*   and fills them with the modified or the original versions of them
+	*   and fills them with the original versions of them
 	* */
-	private fillLiteralsAndPointers():void {
+	private fillLiteralsPointersAndLists():void {
 		this.literals = [];
-		this.tempLiterals = [];
 		this.pointers = [];
-		this.tempPointers = [];
 		this.lists = [];
-		this.tempLists = [];
 
-		// If there are modified literals, assign them ...
-		if( this.property.modifiedLiterals !== void 0 ) {
-			this.literals = this.property.modifiedLiterals;
-			this.tempLiterals = this.property.modifiedLiterals;
-		} else {
-			// ... otherwise fill them with the original values
-			this.property[ this.state ].value.forEach( ( literalOrRDFNode ) => {
-				if( ! RDFLiteral.is( literalOrRDFNode ) ) return;
-				this.literals.push( { copy: literalOrRDFNode } );
-				this.tempLiterals.push( { copy: literalOrRDFNode } );
-			} );
-		}
-
-		// If there are modified pointers, assign them ...
-		if( this.property.modifiedPointers !== void 0 ) {
-			this.pointers = this.property.modifiedPointers;
-			this.tempPointers = this.property.modifiedPointers;
-		} else {
-			// ... otherwise fill them with the original values
-			this.property[ this.state ].value.forEach( ( literalOrRDFNode ) => {
-				if( ! RDFNode.is( literalOrRDFNode ) ) return;
-				this.pointers.push( <PointerRow>{ copy: literalOrRDFNode } );
-				this.tempPointers.push( <PointerRow>{ copy: literalOrRDFNode } );
-			} );
-		}
-
-		// If there are modified literals, assign them ...
-		if( this.property.modifiedLists !== void 0 ) {
-			this.lists = this.property.modifiedLists;
-			this.tempLists = this.property.modifiedLists;
-		} else {
-			// ... otherwise fill them with the original values
-			this.property[ this.state ].value.forEach( ( literalOrRDFNodeOrList ) => {
-				if( ! RDFList.is( literalOrRDFNodeOrList ) ) return;
+		this.property[ this.state ].value.forEach( ( literalOrRDFNodeOrList ) => {
+			if( RDFLiteral.is( literalOrRDFNodeOrList ) ) {
+				this.literals.push( { copy: literalOrRDFNodeOrList } );
+			} else if( RDFNode.is( literalOrRDFNodeOrList ) ) {
+				this.pointers.push( <PointerRow>{ copy: literalOrRDFNodeOrList } );
+			} else if( RDFList.is( literalOrRDFNodeOrList ) ) {
 				this.lists.push( <ListRow>{ copy: literalOrRDFNodeOrList[ "@list" ].map( ( item ) => { return { copy: item } } ) } );
-				this.tempLists.push( <ListRow>{ copy: literalOrRDFNodeOrList } );
-			} );
-		}
+			}
+		} );
 	}
 
 	/*
@@ -263,9 +184,7 @@ export class PropertyContentComponent implements AfterViewInit, OnInit {
 		this.addNewList.emit( true );
 	}
 
-	/*
-	* Checks for changes in the name of the property
-	* */
+
 	checkForChangesOnName( newName:string ):void {
 		this.propertyName = newName;
 		if( (this.propertyName !== void 0) &&
@@ -278,22 +197,21 @@ export class PropertyContentComponent implements AfterViewInit, OnInit {
 	}
 
 	checkForChangesOnLiterals( literals:LiteralStatus[] ):void {
-		this.tempLiterals = literals;
+		this.literals = literals;
 		this.changePropertyContent();
 	}
 
 	checkForChangesOnPointers( pointers:PointerRow[] ):void {
-		this.tempPointers = pointers;
+		this.pointers = pointers;
 		this.changePropertyContent();
 	}
 
 	checkForChangesOnLists( lists:ListRow[] ):void {
 		this.lists = lists;
-		this.tempLists = lists;
 		this.changePropertyContent();
 	}
 
-	convertToListWithStates( lists:ListRow[] ):ListRow[] {
+	private convertToListWithStates( lists:ListRow[] ):ListRow[] {
 		let resultingLists:ListRow[] = [];
 		lists.forEach( ( list:ListRow ) => {
 			let state:string = (! ! list.added) ? "added" : (! ! list.deleted) ? "deleted" : (! ! list.modified) ? "modified" : "copy";
@@ -307,7 +225,7 @@ export class PropertyContentComponent implements AfterViewInit, OnInit {
 		return resultingLists;
 	}
 
-	getRDFList( list:ListRow, state:string ):any[] {
+	private getRDFList( list:ListRow, state:string ):any[] {
 		let resultingListContent:any[] = [];
 		list[ state ].forEach( ( literalOrPointer:any ) => {
 			if( ! ! literalOrPointer.deleted ) return;
@@ -324,59 +242,14 @@ export class PropertyContentComponent implements AfterViewInit, OnInit {
 	changePropertyContent():void {
 		this.tempProperty.id = this.id;
 		this.tempProperty.name = this.propertyName;
-		this.tempProperty.value = this.value;
-
-
-		// If the value of the property is an array, change the values of its literals/pointers/lists ...
-
-		this.tempProperty.value = [];
-		let tempLists:any[] = this.convertToListWithStates( this.tempLists );
-		[]
-			.concat( this.tempLiterals )
-			.concat( this.tempPointers )
-			.concat( tempLists )
-			.forEach( ( literalOrPointerOrList:LiteralStatus | PointerRow | ListRow ) => {
-				if( literalOrPointerOrList.deleted ) return;
-
-				let state:string = (! ! literalOrPointerOrList.modified) ? "modified" : (! ! literalOrPointerOrList.added) ? "added" : "copy";
-
-				this.tempProperty.value.push( literalOrPointerOrList[ state ] );
-			} );
-
-
-		if( this.propertyHasChanged ) {
-
-			if( this.literalsHaveChanged ) {
-				this.property.modifiedLiterals = this.tempLiterals;
-			} else {
-				delete this.property.modifiedLiterals;
-			}
-
-			if( this.pointersHaveChanged ) {
-				this.property.modifiedPointers = this.tempPointers;
-			} else {
-				delete this.property.modifiedPointers;
-			}
-
-			if( this.listsHaveChanged ) {
-				this.property.modifiedLists = this.tempLists;
-			} else {
-				delete this.property.modifiedLists;
-			}
-		}
+		this.tempProperty.value = this.getValueWithChanges();
+		delete this.property.modified;
 
 		if( ! ! this.property.copy ) {
-
-			if( this.propertyHasChanged ) {
-				this.property.modified = this.tempProperty;
-			} else {
-				delete this.property.modified;
-			}
+			this.property.modified = this.tempProperty;
 			this.onChangeProperty.emit( this.tempProperty );
 
 		} else if( ! ! this.property.added ) {
-
-			if( this.tempProperty.name !== this.property.added.name ) {}
 
 			this.id = this.propertyName;
 			this.property.added = this.tempProperty;
@@ -390,8 +263,57 @@ export class PropertyContentComponent implements AfterViewInit, OnInit {
 		}
 	}
 
+	/*
+	*   Creates a new array with the new value created by concatenating
+	*   the original/modified/added literals, pointers and lists
+	* */
+	private getValueWithChanges():Array<LiteralStatus | PointerRow | ListRow> {
+		let tempValue:Array<LiteralStatus | PointerRow | ListRow> = [];
+		let tempLists:ListRow[] = this.convertToListWithStates( this.lists );
+
+		// Concat the literals with the pointers and the lists
+		// and add them to the returning value
+		[ ...this.literals, ...this.pointers, ...tempLists ]
+			.forEach( ( literalOrPointerOrList:LiteralStatus | PointerRow | ListRow ) => {
+				if( literalOrPointerOrList.deleted ) return;
+
+				let state:string = (! ! literalOrPointerOrList.modified) ? "modified" : (! ! literalOrPointerOrList.added) ? "added" : "copy";
+
+				tempValue.push( literalOrPointerOrList[ state ] );
+			} );
+
+		return tempValue;
+	}
+
+	private sanitize( value:string ):string {
+		let sanitized:string = value;
+		let slug:string = this.getSlug( value );
+		let parts:string[] = value.split( slug );
+		if( parts.length > 0 ) sanitized = parts[ 0 ] + this.escape( slug );
+		return sanitized;
+	}
+
+	private getSlug( uri:string ) {
+		return URI.getSlug( uri );
+	}
+
 	private escape( uri:string ):string {
 		return encodeURI( uri );
+	}
+
+	private initializeAccordions():void {
+		this.$element.find( ".ui.accordion" ).accordion();
+	}
+
+	private initializePropertyButtons():void {
+		this.$element.find( ".ui.options.dropdown.button" ).dropdown( {
+			transition: "swing up"
+		} );
+		this.$element.find( ".ui.save-cancel.buttons .dropdown.button" ).dropdown();
+	}
+
+	private initializeDeletionDimmer():void {
+		this.$element.find( ".property.confirm-deletion.dimmer" ).dimmer( { closable: false } );
 	}
 
 }
