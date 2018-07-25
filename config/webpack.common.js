125a160cdf2106d3e6ecb461b447c3a7d5dcdf4a
Alvaro Contreras <alvaro9210@gmail.com>
2018-07-25 16:32:23 -0500

    Change AoT options from string to property style

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -135,10 +135,9 @@ module.exports = function( metadata ) {
 				mainPath: helpers.root( "src/main.ts" ),
 				sourceMap: true,
 				skipCodeGeneration: true,
-
-				"genDir": "compiled",
-				"skipMetadataEmit": true,
-				"typeChecking": true
+				genDir: "compiled",
+				skipMetadataEmit: true,
+				typeChecking: true
 			} ),
 		],
 	}
