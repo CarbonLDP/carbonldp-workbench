762954fd3af83b7a49ce9c2f5619499e794481c6
Alex Serrano <alexserrano63@gmail.com>
2018-08-01 11:03:27 -0500

    Add support for customWidgets on dashboard.view

--- src/app/root-content/dashboard/dashboard.view.ts
+++ src/app/root-content/dashboard/dashboard.view.ts
@@ -1,7 +1,8 @@
-import { Component } from "@angular/core";
-import { Widget } from "./widgets/widgets.component"
+import { Component} from "@angular/core";
+import { CustomWidget, Widget } from "./widgets/widgets.component"
 import "semantic-ui/semantic";
-
+import { WidgetsService } from "app/root-content/dashboard/widgets/widgets.service";
+import { CarbonLDP } from "carbonldp";
 
 @Component( {
 	selector: "cw-dashboard-view",
@@ -9,9 +10,24 @@ import "semantic-ui/semantic";
 	styleUrls: [ "./dashboard.view.scss" ],
 } )
 export class DashboardView {
-	widgetsList:Widget[] = [
-		{ id: 1, name: "totalDocuments", title: "Documents", hide: false },
-		{ id: 2, name: "totalTriples", title: "Triples", hide: false } ];
 
+	widgetsService: WidgetsService;
+	carbonldp: CarbonLDP;
+	constructor( carbonldp:CarbonLDP, widgetsService:WidgetsService ) {
+		this.widgetsService = widgetsService;
+		this.carbonldp = carbonldp;
+	}
+
+	widgetsList: Array<Widget | CustomWidget> = [
+		{ id: 1, name: "totalDocuments", title: "Documents", hide: false, customWidget:false },
+		{ id: 2, name: "totalTriples", title: "Triples", hide: false , customWidget:false} ];
+
+	ngOnInit(){
+		this.getCustomWidgetsOnLocalStorage();
+	}
+
+	public getCustomWidgetsOnLocalStorage() {
+		this.widgetsList = [ ...this.widgetsList , ...this.widgetsService.getCustomWidgetsOnLocalStorage()];
+	}
 }
 
