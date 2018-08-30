976901ac76320bce2b66a0ae9bbf7afb5cae60e3
Miguel Aragón <miguel.aragon@base22.com>
2018-08-30 22:37:45 +0900

    Add partial refactor work

--- src/app/shared/messages-area/error/error-message-generator.ts
+++ src/app/shared/messages-area/error/error-message-generator.ts
@@ -2,23 +2,47 @@ import { Errors } from "carbonldp/HTTP";
 import { JSONLDParser } from "carbonldp/JSONLD";
 import { C } from "carbonldp/Vocabularies";
 
-import { Message, ValidationResult, ValidationDetails, ValidationError, Types } from "../message.component";
+import { Message, Types, ValidationDetails, ValidationError, ValidationResult } from "../message.component";
+import { Response } from "../../../../../../carbonldp-js-sdk/dist/HTTP/Response";
+import { RDFDocument } from "../../../../../../carbonldp-js-sdk/dist/RDF/Document";
+import { RDFNode } from "../../../../../../carbonldp-js-sdk/src/RDF/Node";
 
 
 export class ErrorMessageGenerator {
 
-	public static getErrorMessage( error:Errors.HTTPError | Error ):Message {
-		let errorMessage:Message = {
+	public static getErrorMessage( error:Errors.HTTPError | Error ):Promise<Message> {
+		return Promise.resolve( {
 			title: "",
 			content: "",
 			statusCode: "",
 			statusMessage: "",
 			endpoint: "",
 			type: Types.ERROR,
-		};
+		} )
+			.then( ( errorMessage:Message ) => {
+				if( error instanceof Errors.HTTPError ) {
+					errorMessage.content = errorMessage.content === "" ? this.getFriendlyHTTPMessage( error ) : errorMessage.content;
+					errorMessage.statusCode = error.hasOwnProperty( "message" ) ? "" + error.response.status : "";
+					errorMessage.statusMessage = (<XMLHttpRequest>error.response.request).statusText;
+					errorMessage.title = errorMessage.statusMessage;
+					errorMessage.endpoint = (<any>error.response.request).responseURL;
 
-		errorMessage.title = error.hasOwnProperty( "name" ) ? error.name : "";
-		errorMessage.content = error.hasOwnProperty( "message" ) ? error.message : "";
+					if( this.hasErrorResponse( error ) ) return this.parseErrorResponse( error.response ).then( ( parsedError:{ errorMessage:string, parsedErrorMessage:string } ) => {
+						// FIXME(2018-08-29):
+
+						return errorMessage;
+					} );
+				} else if( error.hasOwnProperty( "stack" ) ) {
+					// If it's an uncaught exception
+					errorMessage.title = error.message;
+					errorMessage.content = error.hasOwnProperty( "message" ) ? error.message : "";
+					errorMessage.stack = error.stack;
+				} else {
+					errorMessage.title = error.hasOwnProperty( "name" ) ? error.name : "";
+					errorMessage.content = error.hasOwnProperty( "message" ) ? error.message : "";
+				}
+				return errorMessage;
+			} );
 
 		// If it's a HTTP error
 		if( error instanceof Errors.HTTPError ) {
@@ -43,7 +67,55 @@ export class ErrorMessageGenerator {
 		return errorMessage;
 	}
 
-	private static getErrors( error:Errors.HTTPError ):Promise<any[]> {
+	private static hasErrorResponse( error:Errors.HTTPError ) {
+		let contentType = error.response.getHeader( "Content-Type" );
+		if( contentType === null ) return false;
+
+		return contentType.hasValue( "application/ld+json" );
+	}
+
+	private static parseErrorResponse( response:Response ):Promise<{ errorMessage:string, parsedErrorMessage:string }> {
+		const combineValues = ( accumulator, currentValue ) => accumulator[ '@value' ]
+			? currentValue[ '@value' ] + " " + accumulator[ '@value' ]
+			: currentValue[ '@value' ] + " " + accumulator;
+
+		return new JSONLDParser().parse( response.data ).then( errorResponse => {
+			let parsedError = {
+				errorMessage: "",
+				parsedErrorMessage: ""
+			};
+
+			let nodes = RDFDocument.getFreeNodes( errorResponse );
+			let errors = nodes
+				.filter( function hasTypeError( node ) {
+					return RDFNode.hasType( node, `${C.namespace}Error` );
+				} )
+				.filter( function hasErrorCode87C8( node ) {
+					if( ! (`${C.namespace}errorCode` in node) ) return false;
+					if( ! Array.isArray( node[ `${C.namespace}errorCode` ] ) || node[ `${C.namespace}errorCode` ].length !== 1 ) return false;
+
+					return node[ `${C.namespace}errorCode` ][ 0 ][ "@value" ] === "0x87C8";
+				} )
+			;
+			if( errors.length !== 1 )
+
+
+			// FIXME(2018-08-29):
+
+			let errors = errorResponse.filter( ( subject ) => {
+				return (! ! subject[ "@type" ] && subject[ "@type" ].indexOf( `${C.namespace}Error` ) !== - 1) || (! ! subject[ `${C.namespace}key` ] && this.objectPropInArray( subject[ `${C.namespace}key` ], "@value", "parserErrorMessage" ));
+			} );
+
+			errors.forEach( ( error ) => {
+				! ! error[ `${C.namespace}errorMessage` ] ? parsedError[ "errorMessage" ] = error[ `${C.namespace}errorMessage` ].length > 1 ? error[ `${C.namespace}errorMessage` ].reduce( combineValues ) : error[ `${C.namespace}errorMessage` ][ 0 ][ '@value' ] : "";
+				! ! error[ `${C.namespace}value` ] ? parsedError[ "parserErrorMessage" ] = error[ `${C.namespace}value` ].length > 1 ? error[ `${C.namespace}value` ].reduce( combineValues ) : error[ `${C.namespace}value` ][ 0 ][ '@value' ] : "";
+			} );
+			return parsedError;
+		} );
+	}
+
+	// FIXME(2018-08-29): This method includes logic that is only relevant to the Document Explorer. Move it to a more appropriate place.
+	private static getErrors( response:Response ):Promise<any[]> {
 		let parser:JSONLDParser = new JSONLDParser();
 		let errors:any[] = [];
 		return parser.parse( error.response.data ).then( ( errorResponse ) => {
@@ -63,30 +135,10 @@ export class ErrorMessageGenerator {
 		} );
 	}
 
-	//parse the error message on JSONLD
-	private static getErrorMessagefromJSONLD( error:Errors.HTTPError ):Promise<any> {
-		let parser:JSONLDParser = new JSONLDParser();
-		let errors:any[] = [];
-		let parsedError = {};
-		const reducer = (accumulator, currentValue) => !!accumulator['@value'] ? currentValue['@value'] + " " + accumulator['@value'] : currentValue['@value'] + " " + accumulator;
-
-		return parser.parse( error.response.data ).then(errorResponse => {
-			errors = errorResponse.filter( ( subject ) => {
-				return (!!subject[ "@type" ] && subject[ "@type" ].indexOf( `${C.namespace}Error` ) !== - 1) || (!!subject[ `${C.namespace}key` ] && this.objectPropInArray(subject[ `${C.namespace}key` ], "@value", "parserErrorMessage"));
-			} );
-			errors.forEach( ( error ) => {
-				!!error[`${C.namespace}errorMessage`] ? parsedError["errorMessage"] =  error[`${C.namespace}errorMessage`].length > 1 ? error[`${C.namespace}errorMessage`].reduce(reducer) : error[`${C.namespace}errorMessage`][0]['@value'] : "";
-				!!error[`${C.namespace}value`] ? parsedError["parserErrorMessage"] =  error[`${C.namespace}value`].length > 1 ? error[`${C.namespace}value`].reduce(reducer) : error[`${C.namespace}value`][0]['@value'] : "";
-			});
-			return parsedError;
-		});
-	}
-
-
-	private static objectPropInArray(list, prop, val):boolean {
-		if (list.length > 0 ) {
-			for (let i in list) {
-				if (list[i][prop] === val) {
+	private static objectPropInArray( list, prop, val ):boolean {
+		if( list.length > 0 ) {
+			for( let i in list ) {
+				if( list[ i ][ prop ] === val ) {
 					return true;
 				}
 			}
