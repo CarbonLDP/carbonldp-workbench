fa85e082682065a53cccb5f601ad978e3160217f
AlexSerrano22 <serrano_ayn@hotmail.com>
2018-09-22 05:18:08 -0500

    Update carbonldp to 5.0.0

--- src/app/workbench/explorer/document-explorer/access-point-creator/access-point-creator.component.ts
+++ src/app/workbench/explorer/document-explorer/access-point-creator/access-point-creator.component.ts
@@ -62,7 +62,7 @@ export class AccessPointCreatorComponent implements AfterViewInit {
 		if( ! ! data.isMemberOfRelation ) baseAccessPoint.isMemberOfRelation = data.isMemberOfRelation;
 		let accessPoint = AccessPoint.create( baseAccessPoint );
 
-		this.carbonldp.documents.get( this.parentURI ).then( ( document:Document ) => {
+		this.carbonldp.documents.$get( this.parentURI ).then( ( document:Document ) => {
 			return this.documentsResolverService.createAccessPoint( document, accessPoint, slug );
 		} ).then( ( document:Document ) => {
 			this.onSuccess.emit( document );
