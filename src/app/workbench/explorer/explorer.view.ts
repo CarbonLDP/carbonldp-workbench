c1169d5b307f681d400d20c52dd48b5fbe49232a
Alex Serrano <alexserrano63@gmail.com>
2018-10-10 11:58:58 -0500

    Add min-height to flex parents of scroll-content wrapper

--- src/app/workbench/explorer/explorer.view.ts
+++ src/app/workbench/explorer/explorer.view.ts
@@ -3,8 +3,9 @@ import { Component, } from "@angular/core";
 @Component( {
 	selector: "cw-explorer-view",
 	templateUrl: "./explorer.view.html",
-	styles: [ ":host { display: block; }" ],
+	styles: [ ":host { display: block; min-height: 0;}" ],
 } )
+//min-height: 0; this is necessary to fix issue in firefox related with flexbox overflow
 export class ExplorerView {
 
 	constructor() {}
