6b0963e18f7a13c07decd9691441784aa1401fee
Alejandro Serrano <alexserrano63@gmail.com>
2018-10-31 22:10:47 -0600

    Add markText typing to CodeMirror Editor interface

--- typings/custom.codemirror/index.d.ts
+++ typings/custom.codemirror/index.d.ts
@@ -1,3 +1,7 @@
 interface Element {
 	CodeMirror:CodeMirror.Editor;
 }
+
+interface Editor {
+	markText( from:CodeMirror.Position, to:CodeMirror.Position, options?:CodeMirror.TextMarkerOptions ):any;
+}
