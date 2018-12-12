38e12260f0bfcf926b9e3e3996795cbc8bd9b240
Miguel Aragón <miguel.aragon@base22.com>
2018-12-12 12:11:44 +0900

    Add cssVar pipe to set css variables easily

--- src/app/common/pipes/pipes.module.ts
+++ src/app/common/pipes/pipes.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // Pipes
+import { CSSVarPipe } from "./css-var.pipe";
 import { GetSlugPipe } from "./get-slug.pipe";
 import { DecodeURIPipe } from "./decode-uri.pipe";
 import { FormatDurationPipe } from "app/common/pipes/format-duration.pipe";
@@ -10,11 +11,13 @@ import { FormatDurationPipe } from "app/common/pipes/format-duration.pipe";
 		CommonModule,
 	],
 	declarations: [
+		CSSVarPipe,
 		GetSlugPipe,
 		DecodeURIPipe,
 		FormatDurationPipe,
 	],
 	exports: [
+		CSSVarPipe,
 		GetSlugPipe,
 		DecodeURIPipe,
 		FormatDurationPipe,
